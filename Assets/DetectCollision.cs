bb90c09b45b4dd0746754579564904636d1d5be9
Unknown <guillaume.devantoy@epitech.eu>
2018-03-01 19:48:52 -0500

    first

---
+++ Assets/DetectCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollision : MonoBehaviour {
+
+    Rigidbody _rb;
+    // Use this for initialization
+    void Start () {
+        _rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            _rb.isKinematic = false;
+        }
+    }
+
+    /*private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Respawn")
+        {
+            _rb.isKinematic = true;
+        }
+    }*/
+}
